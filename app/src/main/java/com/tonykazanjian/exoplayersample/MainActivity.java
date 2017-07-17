8be45a15da856bc7c4ddd8192f00ef54b7bc9b71
Tony Kazanjian <tony.kazanjian@gmail.com>
2017-07-17 14:05:47 -0700

    created layout

---
+++ app/src/main/java/com/tonykazanjian/exoplayersample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tonykazanjian.exoplayersample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.exoplayer.R;
+
+/**
+ * @author Tony Kazanjian
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
